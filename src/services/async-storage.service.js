fa9f38f9b4a479f38c7350bec06c3e6a664ec6e4
ALONGOD <alongoddard1@gmail.com>
2024-07-31 09:23:53 +0300

    Use async await

--- src/services/async-storage.service.js
+++ src/services/async-storage.service.js
@@ -4,71 +4,63 @@ export const storageService = {
   post,
   put,
   remove,
+};
+
+async function query(entityType, delay = 700) {
+  const entities = JSON.parse(localStorage.getItem(entityType)) || [];
+  return new Promise(resolve => setTimeout(() => resolve(entities), delay));
 }
 
-function query(entityType, delay = 700) {
-  var entities = JSON.parse(localStorage.getItem(entityType)) || []
-  return new Promise(resolve => setTimeout(() => resolve(entities), delay))
+async function get(entityType, entityId) {
+  const entities = await query(entityType);
+  const entity = entities.find(entity => entity._id === entityId);
+  if (!entity) {
+    throw new Error(`Get failed, cannot find entity with id: ${entityId} in: ${entityType}`);
+  }
+  return entity;
 }
 
-function get(entityType, entityId) {
-  return query(entityType).then(entities => {
-    const entity = entities.find(entity => entity._id === entityId)
-    if (!entity)
-      throw new Error(
-        `Get failed, cannot find entity with id: ${entityId} in: ${entityType}`
-      )
-    return entity
-  })
+async function post(entityType, newEntity) {
+  newEntity = { ...newEntity };
+  newEntity._id = _makeId();
+  const entities = await query(entityType);
+  entities.push(newEntity);
+  _save(entityType, entities);
+  return newEntity;
 }
 
-function post(entityType, newEntity) {
-  newEntity = { ...newEntity }
-  newEntity._id = _makeId()
-  return query(entityType).then(entities => {
-    entities.push(newEntity)
-    _save(entityType, entities)
-    return newEntity
-  })
+async function put(entityType, updatedEntity) {
+  const entities = await query(entityType);
+  const idx = entities.findIndex(entity => entity._id === updatedEntity._id);
+  if (idx < 0) {
+    throw new Error(`Update failed, cannot find entity with id: ${updatedEntity._id} in: ${entityType}`);
+  }
+  entities.splice(idx, 1, updatedEntity);
+  _save(entityType, entities);
+  return updatedEntity;
 }
 
-function put(entityType, updatedEntity) {
-  return query(entityType).then(entities => {
-    const idx = entities.findIndex(entity => entity._id === updatedEntity._id)
-    if (idx < 0)
-      throw new Error(
-        `Update failed, cannot find entity with id: ${entityId} in: ${entityType}`
-      )
-    entities.splice(idx, 1, updatedEntity)
-    _save(entityType, entities)
-    return updatedEntity
-  })
-}
-
-function remove(entityType, entityId) {
-  return query(entityType).then(entities => {
-    const idx = entities.findIndex(entity => entity._id === entityId)
-    if (idx < 0)
-      throw new Error(
-        `Remove failed, cannot find entity with id: ${entityId} in: ${entityType}`
-      )
-    entities.splice(idx, 1)
-    _save(entityType, entities)
-  })
+async function remove(entityType, entityId) {
+  const entities = await query(entityType);
+  const idx = entities.findIndex(entity => entity._id === entityId);
+  if (idx < 0) {
+    throw new Error(`Remove failed, cannot find entity with id: ${entityId} in: ${entityType}`);
+  }
+  entities.splice(idx, 1);
+  _save(entityType, entities);
 }
 
 // Private functions
 
 function _save(entityType, entities) {
-  localStorage.setItem(entityType, JSON.stringify(entities))
+  localStorage.setItem(entityType, JSON.stringify(entities));
 }
 
 function _makeId(length = 5) {
-  var text = ''
-  var possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return text
+  return text;
 }
--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,48 +1,27 @@
-import Axios from 'axios'
+import Axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'production'
   ? '/api/'
-  : '//localhost:3030/api/'
+  : '//localhost:3030/api/';
 
 const axios = Axios.create({
   withCredentials: true
-})
+});
 
 export const httpService = {
-  get(endpoint, data) {
-    return ajax(endpoint, 'GET', data)
+  async get(endpoint, data) {
+    return ajaxWithAsyncAwait(endpoint, 'GET', data);
   },
-  post(endpoint, data) {
-    return ajax(endpoint, 'POST', data)
+  async post(endpoint, data) {
+    return ajaxWithAsyncAwait(endpoint, 'POST', data);
   },
-  put(endpoint, data) {
-    return ajax(endpoint, 'PUT', data)
+  async put(endpoint, data) {
+    return ajaxWithAsyncAwait(endpoint, 'PUT', data);
   },
-  delete(endpoint, data) {
-    return ajax(endpoint, 'DELETE', data)
+  async delete(endpoint, data) {
+    return ajaxWithAsyncAwait(endpoint, 'DELETE', data);
   }
-}
-
-function ajax(endpoint, method = 'GET', data = null) {
-  return axios({
-    url: `${BASE_URL}${endpoint}`,
-    method,
-    data,
-    params: (method === 'GET') ? data : null
-  })
-    .then(res => res.data)
-    .catch(err => {
-      console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data:`, data)
-      console.dir(err)
-      if (err.response && err.response.status === 401) {
-        sessionStorage.clear()
-        window.location.assign('/')
-      }
-      throw err
-    })
-}
-
-
+};
 
 async function ajaxWithAsyncAwait(endpoint, method = 'GET', data = null) {
   try {
@@ -51,19 +30,19 @@ async function ajaxWithAsyncAwait(endpoint, method = 'GET', data = null) {
       method,
       data,
       params: (method === 'GET') ? data : null
-    })
-    return res.data
+    });
+    return res.data;
   } catch (err) {
-    console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data:`, data)
-    console.dir(err)
+    console.log(`Had issues ${method}ing to the backend, endpoint: ${endpoint}, with data:`, data);
+    console.dir(err);
     if (err.response && err.response.status === 401) {
-      sessionStorage.clear()
-      window.location.assign('/')
-      // Depends on routing startegy - hash or history
+      sessionStorage.clear();
+      window.location.assign('/');
+      // Depending on routing strategy - hash or history
       // window.location.assign('/#/login')
       // window.location.assign('/login')
       // router.push('/login')
     }
-    throw err
+    throw err;
   }
 }
